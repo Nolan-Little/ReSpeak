a6e99ecba029803a23887c980eed78e41e9cb340
Nolan-Little <little.nolan@gmail.com>
2018-12-06 11:05:26 -0600

    can select collection to view its note titles

--- src/app/ApplicationViews.js
+++ src/app/ApplicationViews.js
@@ -1,30 +1,14 @@
 import React, { Component } from 'react'
 import Dashboard from '../components/dashboard/dashboard'
-import userSession from './../modules/userSession'
-import api from './../modules/apiManager'
+
 
 
 export default class ApplicationViews extends Component {
-  constructor(){
-    super()
-    this.state={
-      collections: []
-    }
-  }
-
-  getUserData = (user) => {
-    api.getData(`collections?userId=${user}&_embed=notes`)
-      .then((collections) => this.setState({collections:collections}))
-  }
-
-  componentDidMount(){
-    this.getUserData(userSession.getUser())
-  }
 
   render(){
     return (
       <React.Fragment>
-        <Dashboard successfulLogout={this.props.successfulLogout} collections={this.state.collections}/>
+        <Dashboard successfulLogout={this.props.successfulLogout}/>
       </React.Fragment>
     )
   }
--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react'
 import { Button, Row, Col, Container, ListGroupItem } from 'reactstrap';
 import NoteGroup from './noteGroup'
-
+import userSession from './../../modules/userSession'
+import api from './../../modules/apiManager'
 export default class Dashboard extends Component {
 
   constructor() {
     super()
     this.state = {
-      currentCollection: "initial"
+      currentCollection: "initial",
+      currentTitle: null,
+      collections: []
     }
+  }
+
+  getUserData = (user) => {
+    return api.getData(`collections?userId=${user}&_embed=notes`)
+      .then((collections) => this.setState({ collections: collections }))
+  }
+
+  componentDidMount() {
+    this.getUserData(userSession.getUser())
+      .then(() => this.setInitialTitle())
+
+  }
+
+
+  selectCollection = (id) => {
+    this.setState({ currentCollection: id })
+  }
+
+  setInitialTitle = () => {
+    if (this.state.currentCollection === "initial") {
+      this.setState({ currentTitle: this.state.collections[0].title })
+    }
+  }
+
+  setCurrentTitle = (title) => {
+      this.setState({ currentTitle: title })
   }
 
 
@@ -22,17 +51,20 @@ export default class Dashboard extends Component {
           <Col sm={{ size: 'auto', offset: 2 }}><Button className="m-2" onClick={() => this.props.successfulLogout()}>Logout</Button></Col>
         </Row>
         <Container className="m-5">
-          <h1 className="text-center">Collection Title</h1>
+          <h1 className="text-center">{this.state.currentTitle}</h1>
           <Row>
             <Col xs="3">
               {
-                this.props.collections.map((col) => {
-                  return <ListGroupItem id={col.id} key={col.id}>{col.title}</ListGroupItem>
+                this.state.collections.map((col) => {
+                  return <ListGroupItem onClick={() => {
+                    this.selectCollection(col.id)
+                    this.setCurrentTitle(col.title)
+                  }} key={col.id}>{col.title}</ListGroupItem>
                 })
               }
             </Col>
             <Col xs="9">
-              <NoteGroup currentCollection={this.state.currentCollection} collections={this.props.collections} />
+              <NoteGroup currentCollection={this.state.currentCollection} collections={this.state.collections} />
             </Col>
           </Row>
         </Container>
