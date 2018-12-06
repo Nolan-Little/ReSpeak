00f64f0ccd406b1f5cd954ad7ac836d29e353896
Nolan-Little <little.nolan@gmail.com>
2018-12-06 12:02:04 -0600

    Clicking edit enables edit view

--- src/components/note/noteDetails.js
+++ src/components/note/noteDetails.js
@@ -1,21 +1,59 @@
 import React, { Component } from 'react'
-import {Modal, ModalBody, ModalHeader, ModalFooter, Button} from 'reactstrap'
+import { Input, Modal, ModalBody, ModalHeader, ModalFooter, Button } from 'reactstrap'
 
 export default class NoteDetails extends Component {
-  render () {
+  constructor() {
+    super()
+    this.state = {
+      editing: false
+    }
+  }
+
+  toggleEditing = () => {
+    this.setState({ editing: !this.state.editing })
+  }
+
+  handleFieldChange = (evt) => {
+    const stateToChange = {}
+    stateToChange[evt.target.id] = evt.target.value
+    this.setState(stateToChange)
+  }
+
+  render() {
     return (
-      <Modal isOpen={this.props.modal} toggle={this.props.toggle} className={this.props.className}>
-      <ModalHeader toggle={this.props.toggle}>{this.props.note.title}</ModalHeader>
-      <ModalBody>
-        {this.props.note.textContent}
-      </ModalBody>
-      <ModalFooter>
-        <audio controls></audio>
-        <Button color="primary">Edit</Button>{' '}
-        <Button color="primary">Delete</Button>{' '}
-        <Button color="secondary" onClick={()=>this.props.toggle()}>Save</Button>
-      </ModalFooter>
-    </Modal>
+
+      this.state.editing
+
+        ?
+            // ALLOW EDITING
+        <Modal isOpen={this.props.modal} toggle={this.props.toggle} className={this.props.className}>
+          <ModalHeader toggle={this.props.toggle}>
+            <Input id="title" type="text" defaultValue={this.props.note.title}></Input>
+          </ModalHeader>
+          <ModalBody>
+            <Input id="textContent" type="text" defaultValue={this.props.note.textContent}></Input>
+          </ModalBody>
+          <ModalFooter>
+            <audio controls></audio>
+            <Button color="primary" onClick={() => this.toggleEditing()}>Save Changes</Button>{' '}
+            <Button color="secondary" onClick={() => this.toggleEditing()}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+
+        :
+          // NOT EDITING
+        <Modal isOpen={this.props.modal} toggle={this.props.toggle} className={this.props.className}>
+          <ModalHeader toggle={this.props.toggle}>{this.props.note.title}</ModalHeader>
+          <ModalBody>
+            {this.props.note.textContent}
+          </ModalBody>
+          <ModalFooter>
+            <audio controls></audio>
+            <Button color="primary" onClick={() => this.toggleEditing()}>Edit</Button>{' '}
+            <Button color="primary">Delete</Button>{' '}
+            <Button color="secondary" onClick={() => this.props.toggle()}>Save</Button>
+          </ModalFooter>
+        </Modal>
     )
   }
 }
