5c90305a29bbf3ddb5d31a5b24653ad6b771b333
Nolan-Little <little.nolan@gmail.com>
2018-12-07 16:01:59 -0600

    import firebase files

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -6,7 +6,8 @@ class Firebase {
   constructor(config) {
     let app = firebase.initializeApp(config)
 
-    this.audioStorage = app.storage();
+    this.audioStorage = app.storage()
+    this.audioFilePath = app.storage().ref().child("audio.ogg");
   }
 }
 
