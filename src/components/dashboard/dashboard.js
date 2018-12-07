27a1445ce75ec9ff0c228e3b6bae1c2d143c6790
Nolan-Little <little.nolan@gmail.com>
2018-12-07 10:28:19 -0600

    delete button changes status of collection in databse to deleted true

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -50,6 +50,15 @@ export default class Dashboard extends Component {
       .then(() => this.getUserData(userSession.getUser()))
   }
 
+  deleteCollection = (id) => {
+    return api.editData("collections", { deleted: true }, id)
+      .then(() => this.getUserData(userSession.getUser()))
+      .then(() => {
+        this.setState({ currentCollection: "initial" })
+        this.setInitialTitle()
+      })
+  }
+
 
   // COLLECTION STATE
   selectCollection = (id) => {
@@ -104,18 +113,28 @@ export default class Dashboard extends Component {
             toggle={this.toggleNoteForm}
             modal={this.state.newNoteModal} />
           <Row>
-            <Col xs="3">
+            <Col xs="4">
               {/* create list of collection titles */}
               {
                 this.state.collections.map((col) => {
-                  return <ListGroupItem onClick={() => {
-                    this.selectCollection(col.id)
-                    this.setCurrentTitle(col.title)
-                  }} key={col.id}>{col.title}</ListGroupItem>
+                  return <ListGroupItem
+                    onClick={() => {
+                      this.selectCollection(col.id)
+                      this.setCurrentTitle(col.title)
+                    }}
+                    key={col.id}>
+                    <Col xs="auto">
+                      {col.title}
+                    </Col>
+                    <Col xs="2">
+                      <Button className="m-1">Edit</Button>
+                      <Button onClick={() => this.deleteCollection(col.id)} className="m-1">Delete</Button>
+                    </Col>
+                  </ListGroupItem>
                 })
               }
             </Col>
-            <Col xs="9">
+            <Col xs="8">
               <NoteGroup
                 deleteNote={this.deleteNote}
                 editNote={this.editNote}
